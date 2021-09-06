c25ed766fd8efde2b9a14088cc0c7fd1b066c5b2
Mark <mark.balazon@gmail.com>
2021-09-06 11:55:34 -0400

    added content to SidePanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,11 +3,26 @@ import React from "react";
 const SidePanel = () => {
   return (
     <section id="sidebar">
-      <section id="intro"></section>
+      <section id="intro">
+        <Link to="/" className="profilePic">
+          <img src="../../../public/imgs/profilePic.jpg" alt="" />
+        </Link>
+        <header>
+          <h2>Mark Balazon</h2>
+          <p>
+            <a href="mailto:mark.balazon@gmail.com">mark.balazon@gmail.com</a>
+          </p>
+        </header>
+      </section>
 
-      <section id="blurb"></section>
+      <section id="blurb">
+        <h2>About</h2>
+        <p>my about</p>
+      </section>
 
-      <section id="footer"></section>
+      <section id="footer">
+        <p className="copyright"> &copy; Mark Balazon</p>
+      </section>
     </section>
   );
 };
